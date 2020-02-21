7a6b1d81de1ea1dbb5e87c5590d9ab9beb386b21
ifiamawood <821371350@qq.com>
2020-02-21 20:55:19 +0800

    素数没有1

--- printPrimeNumber/printPrimeNumber.cpp
+++ printPrimeNumber/printPrimeNumber.cpp
@@ -2,10 +2,29 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
+#include "stdlib.h"
 
+using namespace std;
+
+int PrimeNum(int i) {  //判断一个数是否为素数
+	int n =2 ;
+	for (n ; n <= i-1; n++) {
+			if (i%n == 0) {
+				return 1;
+			}
+		}
+	return 0;
+}
 
 int main()
 {
-    return 0;
+	int m=0;
+	for (int i = 2 ; i <= 100; i++) {
+		int m = PrimeNum(i);
+		if ( m ==0 )
+			cout << "PrimeNumber is:" << i << endl;
+	}
+    return 0; 
 }
 
